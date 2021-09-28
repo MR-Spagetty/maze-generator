a16b6977e775ffc6d817f9388f216f87039846af
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2021-09-28 17:19:02 +1300

    implemented display system

--- display_system.py
+++ display_system.py
@@ -8,8 +8,20 @@ class display:
         self.fill_tile = select_type(type)['block']
 
     def print(self, tiles, x, y, player_tile):
+        """Prints the currently visable tiles
+
+        Args:
+            tiles (2D dict): the dictionary containing the tile objects
+
+            x (int): the x coordinate of the player
+
+            y (int): the y coordinate of the player
+
+            player_tile (list): the display of the player's current tile
+        """
+        print(x, y)
         matrix = {}
-        for dis_y in range(y + self.view_distance, y - self.view_distance - 1):
+        for dis_y in range(y + self.view_distance, y - self.view_distance - 1, -1):
             matrix[dis_y] = [
                 [],
                 [],
@@ -20,12 +32,15 @@ class display:
             for dis_x in range(x - self.view_distance, x +
                                self.view_distance + 1):
                 if (dis_x, dis_y) != (x, y):
-                    for line_id, line_tile in range(len(matrix[dis_y])), tiles[
-                            dis_y][dis_x]:
+                    for line_id, line_tile in zip(range(len(matrix[dis_y])),
+                                                  tiles[dis_y][dis_x].display):
                         matrix[dis_y][line_id] += line_tile
                 else:
-                    for line_id, line_tile in range(len(matrix[dis_y])),\
-                            player_tile:
+                    for line_id, line_tile in zip(range(len(matrix[dis_y])),
+                                                  player_tile):
                         matrix[dis_y][line_id] += line_tile
-            matrix[y] = '\n'.join(matrix)[y]
-        print('\n'.join())
+                matrix_y = []
+            for line in matrix[dis_y]:
+                matrix_y.append(''.join(line))
+            matrix[dis_y] = '\n'.join(matrix_y)
+        print('\n'.join(matrix.values()))
